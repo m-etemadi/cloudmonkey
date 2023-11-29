7719b6e49961b5775e6a41a64c777849fd288ea8
m-etemadi <m.etemadi92@gmail.com>
2023-11-29 20:21:07 +1100

    Improvements

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -6,7 +6,7 @@ function Contact({ children }) {
   return (
     <section className={`${styles.contact} py-8`}>
       <header>
-        <h2 className="heading heading-secondary color-light">Get In Touch</h2>
+        <h2 className="heading heading-secondary color-red">Get In Touch</h2>
       </header>
       <div className="container">
         <div className={styles.contactContent}>{children}</div>
