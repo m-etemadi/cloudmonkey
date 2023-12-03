3c0a049d26080ff966a0a5103e8ba899c6f3f967
m-etemadi <m.etemadi92@gmail.com>
2023-12-03 13:30:33 +1100

    Contact section done

--- src/components/Contact/Info.jsx
+++ src/components/Contact/Info.jsx
@@ -1,5 +1,27 @@
+import phoneIcon from '../../assets/images/icons/phone-icon.png';
+import mailIcon from '../../assets/images/icons/mail-icon.png';
+
+import styles from './Contact.module.css';
+
 function Info() {
-  return <div className="contact-info"></div>;
+  return (
+    <div className={styles.contactInfo}>
+      <div className={styles.item}>
+        <div className="icon">
+          <img src={phoneIcon} alt="+49 316 20 20" />
+        </div>
+        <a href="tel:+493162020">+49 316 20 20</a>
+      </div>
+      <div className={styles.item}>
+        <div className="icon">
+          <img src={mailIcon} alt="support@cloudmonkey.com.au" />
+        </div>
+        <a href="mailto:support@cloudmonkey.com.au">
+          support@cloudmonkey.com.au
+        </a>
+      </div>
+    </div>
+  );
 }
 
 export default Info;
