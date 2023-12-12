d9948a858b1a706c1c848a0a49991e9b14570fa9
m-etemadi <m.etemadi92@gmail.com>
2023-12-12 13:15:25 +1100

    Improvements and bug fixes

--- src/components/Contact/Form.jsx
+++ src/components/Contact/Form.jsx
@@ -1,25 +1,74 @@
-import styles from './Contact.module.css';
+import { useState } from 'react';
+
+import { URL } from '../../config/config';
 
 import Button from '../common/Button/Button';
 
+import styles from './Contact.module.css';
+
 function Form() {
+  const [error, setError] = useState('');
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+
+    try {
+      const formData = new FormData(e.target);
+      const res = await fetch(URL, {
+        method: 'POST',
+        body: formData,
+        headers: {
+          Accept: 'application/json',
+        },
+      });
+      if (!res.ok) throw new Error('Something went wrong!');
+      const data = await res.json();
+
+      if (data.success) {
+        setError('Message sent!');
+      }
+    } catch (err) {
+      setError(err.message);
+    }
+  }
+
   return (
     <div className={styles.contactForm}>
-      <form>
+      <form method="POST" className="form myb-8" onSubmit={handleSubmit}>
         <div>
-          <input type="text" placeholder="Full Name" />
+          <input
+            name="Full Name"
+            type="text"
+            placeholder="Full Name *"
+            required
+          />
         </div>
         <div>
-          <input type="email" placeholder="Email Address" />
+          <input
+            name="Email"
+            type="email"
+            placeholder="Email Address *"
+            required
+          />
         </div>
         <div>
-          <input type="text" placeholder="Subject" />
+          <input name="Subject" type="text" placeholder="Subject *" required />
         </div>
         <div>
-          <textarea rows="4" cols="50" placeholder="Your Message..."></textarea>
+          <textarea
+            name="Message"
+            rows="4"
+            cols="50"
+            placeholder="Your Message... *"
+            required
+          ></textarea>
         </div>
 
-        <Button type="primary">Send</Button>
+        <Button isSubmit={true} type="primary">
+          Send
+        </Button>
+
+        {error && <div className={styles.message}>{error}</div>}
       </form>
     </div>
   );
--- src/components/common/Button/Button.jsx
+++ src/components/common/Button/Button.jsx
@@ -2,8 +2,8 @@ import { Link } from 'react-scroll';
 
 import styles from './Button.module.css';
 
-function Button({ isLink = false, to, type, children }) {
-  if (isLink)
+function Button({ isLink = false, isSubmit = false, to, type, children }) {
+  if (isLink) {
     return (
       <Link
         className={`${styles.btn} ${styles[type]}`}
@@ -15,10 +15,17 @@ function Button({ isLink = false, to, type, children }) {
         {children}
       </Link>
     );
-
-  return (
-    <button className={`${styles.btn} ${styles[type]}`}>{children}</button>
-  );
+  } else if (isSubmit) {
+    return (
+      <button type="submit" className={`${styles.btn} ${styles[type]}`}>
+        {children}
+      </button>
+    );
+  } else {
+    return (
+      <button className={`${styles.btn} ${styles[type]}`}>{children}</button>
+    );
+  }
 }
 
 export default Button;
